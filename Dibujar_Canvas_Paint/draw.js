9de7c5fceba78751d60c8f181f004fdcfd6413c7
Johan Ussa Rubio <johanuss0405@gmail.com>
2022-02-13 20:07:15 -0500

    Aplicacion que permite dibujar estilo Paint terminado

--- Dibujar_Canvas_Paint/draw.js
+++ Dibujar_Canvas_Paint/draw.js
@@ -1,45 +1,81 @@
 let keys = { UP: 38, DOWN: 40, LEFT: 37, RIGHT: 39 }
+let eraserMouse = 0;
 let posX = 500;
 let posY = 275;
 let moveMouse;
+let size;
 
 let canvasDraw = document.getElementById("space-draw");
+let backGround = document.getElementById("input-back");
+let btnEraser = document.getElementById("btn.eraser");
 let btnReset = document.getElementById("btn-clear");
 let pencil = document.getElementById("size-pencil");
 let penCol = document.getElementById("input-color");
-let back = document.getElementById("input-back");
+let label = document.getElementById("label-size");
+
 let lienzo = canvasDraw.getContext("2d");
 
-let backColor = back.value;
+let backColor = backGround.value;
 let sizePencil = pencil.value;
 let pencilColor = penCol.value;
 
+document.addEventListener("keyup", drawKeys);
 canvasDraw.addEventListener("mouseleave", () => { moveMouse = 0; });
 pencil.addEventListener("change", e => { sizePencil = e.target.value; });
 penCol.addEventListener("change", e => { pencilColor = e.target.value; });
-btnReset.addEventListener("click", () => { canvasDraw.width = canvasDraw.width; })
-document.addEventListener("keyup", drawKeys);
+btnReset.addEventListener("click", () => { canvasDraw.width = canvasDraw.width; });
 
-back.addEventListener("change", e => { 
+backGround.addEventListener("change", e => {     
     backColor = e.target.value;
     canvasDraw.style.backgroundColor = backColor;
 });
+btnEraser.addEventListener("click", () => { 
+
+    if (btnEraser.innerHTML == "Borrador") {    
+        eraserMouse = 1;    
+        btnEraser.innerHTML = "Borrador Activo!";  
+        btnEraser.style.border = "5px groove red";
+        label.innerHTML = "Tamaño Borrador";
+    } else {
+        eraserMouse = 0;    
+        btnEraser.innerHTML = "Borrador"; 
+        btnEraser.style.border = "2px outset gray";
+        label.innerHTML = "Tamaño de Pincel";
+    }
+});
 canvasDraw.addEventListener("mousedown", (e) => {
-    moveMouse = 1;
-    posX = e.layerX;
-    posY = e.layerY;
-    drawLine(pencilColor, posX, posY, posX - 2, posY - 2);
+    
+    moveMouse = 1;  
+    size = parseInt(sizePencil) + 2;
+    if (eraserMouse) {
+        posX = e.layerX;
+        posY = e.layerY;
+        lienzo.clearRect(posX, posY, size, size);
+    } else {
+        posX = e.layerX;
+        posY = e.layerY;
+        drawLine(pencilColor, posX, posY, posX - 2, posY - 2);
+    }
 });
 canvasDraw.addEventListener("mouseup", (e) => {
-    moveMouse = 0;
-    posX = e.layerX;
-    posY = e.layerY;
-});
-canvasDraw.addEventListener("mousemove", (e) => {
-    if (moveMouse) {
-        drawLine(pencilColor, posX, posY, e.layerX, e.layerY);
+
+    if (eraserMouse) { moveMouse = 0; } 
+    else {
+        moveMouse = 0;
         posX = e.layerX;
-        posY = e.layerY;    
+        posY = e.layerY;        
+    }
+});
+canvasDraw.addEventListener("mousemove", (e) => {    
+
+    size = parseInt(sizePencil) +  2;
+    if (moveMouse) {        
+        if (eraserMouse) { lienzo.clearRect(e.layerX, e.layerY, size, size); } 
+        else {
+            drawLine(pencilColor, posX, posY, e.layerX, e.layerY);
+            posX = e.layerX;
+            posY = e.layerY;    
+        }
     } 
 });
 
