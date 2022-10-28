79d528401d07420bb95f88c9351e0deba0c7df50
Johan Sebastian Ussa Rubio <90299805+johanussa@users.noreply.github.com>
2022-10-28 10:27:06 -0500

    Se añade métodos find y some

--- MetodosConObjetosJS.js
+++ MetodosConObjetosJS.js
@@ -24,6 +24,34 @@ console.log(arrayObj);
 
 // -> [ [ 'Nombre', 'Johan' ], [ 'Apellido', 'Ussa' ], [ 'Edad', 23 ] ]
 
+/* => Metodo .find() = Retorna true o false si el parametro que le enviamos se encuentra 
+   entre las propiedades del objeto */
+
+var articulos = [
+    { nombre: "Computador", valor: 2600 },
+    { nombre: "Portatil", valor: 3200 },
+    { nombre: "Celular", valor: 1200 },
+    { nombre: "Mouse", valor: 750 },
+    { nombre: "Teclado", valor: 860 },
+    { nombre: "Monitor", valor: 1400 },
+];
+
+articulos.find((element) => {
+    return element.nombre === "Mouse";
+}); // {nombre: 'Mouse', valor: 750} - Retorna la primera ocurrencia, para traer mas elementos 
+//     es conveniente usar el metodo filter() que retorna todos los elementos que cumplan la condicion
+
+articulos.filter((element) => {
+	return element.valor <= 1000;
+}); // (2) [{…}, {…}] - 0 : {nombre: 'Mouse', valor: 750} - 1 : {nombre: 'Teclado', valor: 860}
+
+var artCaros = articulos.some((element) => {  // Funcion que retorna true o false
+	return element.valor >= 3000;	  // dependiendo si se cumple o no la condicion
+}); 
+// Retor solamente valores true o false, en este caso retorna true, ya que si hay 
+// articulos cuyo valor es mayor o igual a 3000, es como si se le preguntara:
+// Hay elementos cuyo valor es mayor o igual a 3000? -> true
+
 /* => Metodo getOwnPropertyDescriptors() -> Retorna un objeto con la descripcion 
       de todas las propiedades que posee el objeto */ 
 
