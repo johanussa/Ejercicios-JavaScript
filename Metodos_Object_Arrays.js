c5b36f5b4937284fe7e438c8b4800b361496ac07
Johan Sebastian Ussa Rubio <90299805+johanussa@users.noreply.github.com>
2022-11-11 21:33:22 -0500

    Update metodos arrays and objects
    Se agregan metodos para manejos de arrays y objetos

--- Metodos_Object_Arrays.js
+++ Metodos_Object_Arrays.js
@@ -43,6 +43,21 @@ data.forEach(e => {
     document.write('Nombre: ', e.Nombre, ' ', e.Apellido, '<br/>');
 });
 
+const newObject = data.map(({Nombre, Apellido, Edad}) => ({
+    Usuario : `${Nombre} ${Apellido}`,
+    Edad
+}))
+    .filter(e => e.Edad > 18)
+    .sort((a, b) => a - b)
+    .reduce((promedio, e) => promedio + e.Edad / 3, 0); 
+
+// Se pueden agregar mas metodos que se apliquen a esa misma variablelas que se requieran
+// En este caso se filtra que los resultados sean mayores a 18 y ademas 
+// Se ordena el objeto alfabeticamente
+// Se tiene el promedio de la edad
+
+console.log(newObject);
+
 const arrayFilter = data.filter(e => e.Nombre !== "Johan");
 console.log(arrayFilter);
 
@@ -84,15 +99,90 @@ const newData = {
     Apellido: 'Ussa',
     Edad: 25
 };
-data.push(newData);
 
-// const find = data.find(e => e.Nombre === newData.Nombre);
-// if(!find) { data.push(newData); }
+const find = data.find(e => e.Nombre === newData.Nombre);
+if(!find) { data.push(newData); }
 
 // Set me permite crear nuevos contenidos donde no se repitan elementos dentro del mismo 
 // Esto se convierte en elemento de tipo Set, pero se puede convertir en un arreglo con 
 //  el objeto Array y su metodo .from()
 
-const totalData = Array.from(new Set([1,2,3,4,5,6,7,8,7,6,5,4,2,5,2]));
+const totalData = Array.from(new Set([1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 2, 5, 2]));
+console.log(totalData); 
 
-console.log(totalData);
+/* Metodo sort() para ordenar objetos - aqui ordenamos el objeto data por sus edades de menor a mayor
+por parametro se envian dos argumentos, a que es la primera posicion del objeto y b es el segundo
+asi los compara y cuando es mayor devuelve 1 y sino -1 de esta forma la funcion ordena el objeto
+Es lo mismo decir que si a un numero le resta otro si queda residuo sera porque el primer numero es mayor
+*/
+
+//const dataSort = data.sort((a, b) => a.Edad > b.Edad ? 1 : -1);
+const dataSort = data.sort((a, b) => a.Edad - b.Edad);
+console.log(dataSort);
+const nums = [1,5,6,9,8,12,14,8,3];
+const numsSort = nums.sort((a, b) => a - b); // Ordenar numeros
+console.log(numsSort);
+
+/*
+ Metodo some() Me sirve cuando necesito saber si existe una propiedad o un dato dentro del objeto
+ sin necesidad de que retorne un array o un elemento sino solo un true o false si el elemento esta o no
+ retorna true si algun elemento cumple con la condicion 
+*/
+
+let findData = data.some((e) => e.Nombre === "Johann");
+console.log(findData);
+findData = data.some(e => e.Edad < 18);
+console.log(findData); 
+
+/*
+ Metodo every() sirve para indicar que todos los elementos del objeto cumplan esa condicion
+ NO solo alguno como lo hace some() que si solo un elemento cumple la condicion es true
+ En este metodo la condicion debe cumplirse en todos los elementos
+*/
+
+const findEvery = data.every(e => e.Edad > 18);
+console.log(findEvery); 
+// => false ya que no todos los usuarios son mayores de 18 años
+const every = data.every(e => e.Nombre.includes('J'));
+console.log(every);
+// => false ya que evalua si en el nombre todos contienen la letra J
+
+/*
+async / await : Solicitar datos a un Backend 
+biblioteca fetch de JS que permite entrar a una direccion en internet 
+JSON placeholder permite poder tener datos de ejemplos entrar en Resources y en post 
+y nos redirecciona a una pagina en donde nos devuelve datos tipo JSON 
+*/
+
+// let dataJson = fetch('https://jsonplaceholder.typicode.com/posts');
+// console.log(dataJson);
+// retorna una Promise{} porque estas consultas toman tiempo 
+
+const ul = document.createElement('ul');
+document.body.append(ul);
+
+// Esta es la forma antigua de realizar consultas 
+
+// let dataJson = fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then((res) => res.json()) // esta conversion es asincrona
+//     .then((data) => {  // y la respuesta se recibe aqui
+//         data.map(e => { 
+//             const li = document.createElement('li');
+//             li.innerText = e.title;
+//             ul.append(li);
+//         });
+//     }); 
+
+// Forma moderna de hacer petiiones a un servidor con async await
+
+async function loadData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    data.forEach(e => {
+        const li = document.createElement('li');
+            li.innerText = e.title;
+            ul.append(li);
+    });
+}
+
+loadData();
