f418310f954f3b93adc9b7803c2ebd3d6e1a3b72
Johan Sebastian Ussa Rubio <90299805+johanussa@users.noreply.github.com>
2022-12-29 17:47:11 -0500

    Se mejora la Aplicación con LocalStorage

--- Paleta_Colores/script.js
+++ Paleta_Colores/script.js
@@ -1,11 +1,40 @@
+const title = document.getElementById('title');
 const colorPal = document.getElementById('colorPal');
+const ulColors = document.getElementById('ulColors');
 const container = document.getElementById('container');
+const saveColor = document.getElementById('saveColor');
 const valueColor = document.getElementById('valueColor');
+const cantColors = document.getElementById('cantColors');
 
-valueColor.innerText = colorPal.value;
-container.style.background = colorPal.value;
+const storageValue = localStorage.getItem('colorValue') || '#000000';
+const dataColors = JSON.parse(localStorage.getItem('colors')) || [];
 
-colorPal.addEventListener('input', (e) => {
-    valueColor.innerText = colorPal.value.toUpperCase();
-    container.style.background = colorPal.value;
+const loadData = (value) => {
+    valueColor.innerText = value.toUpperCase();
+    container.style.background = value;
+    colorPal.value = value;
+    title.style.color = value;
+}
+loadData(storageValue);
+const loadColors = () => {
+    if (dataColors.length) {
+        ulColors.innerText = '';
+        cantColors.innerText = dataColors.length;
+
+        dataColors.forEach(e => {
+            const li = document.createElement('li');
+            li.innerText = e;
+            li.style.background = e;
+            ulColors.append(li);
+        });
+    }
+}
+loadColors();
+
+saveColor.addEventListener('click', () => {
+    dataColors.push(colorPal.value);
+    localStorage.setItem('colors', JSON.stringify(dataColors));
+    loadColors();
 });
+colorPal.addEventListener('input', (e) => loadData(e.target.value));
+colorPal.addEventListener('change', () => localStorage.setItem('colorValue', colorPal.value.toUpperCase()));
