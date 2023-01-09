a1afde8e4af33238f81830b927c7c92a415ec6f5
Johan Sebastian Ussa Rubio <90299805+johanussa@users.noreply.github.com>
2023-01-09 15:30:55 -0500

    Update script.js

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -8,38 +8,35 @@ let resOK = false;
 let newOperation = false;
 
 btns.addEventListener('click', (event) => {
-
     let valor = event.target.value;
-    if (resOK) {
-        if (!operadores.some(e => e === valor)) { inpValor.innerHTML = 0; }
-        acumulador.innerHTML = '';
-        resOK = false;
-    }
-    if (newOperation) {
-        inpValor.innerHTML = 0;
-        newOperation = false;
-    }
-    if (inpValor.innerHTML === '0' && valor !== '.') { inpValor.innerHTML = ''; }
-
-    switch (valor) {
-        case 'c': reset(); break;
-        case 'eraser': eraser(); break;
-        case '+':
-        case '-':
-        case '*':
-        case '/':
+    if (valor !== undefined) {
+        if (resOK) {
+            if (!operadores.some(e => e === valor)) { inpValor.innerHTML = 0; }
+            acumulador.innerHTML = '';
+            resOK = false;
+        }
+        if (newOperation) {
+            inpValor.innerHTML = 0;
+            newOperation = false;
+        }
+        if (inpValor.innerHTML === '0' && valor !== '.') { inpValor.innerHTML = ''; }
+    
+        const ACTIONS = {
+            c: () => reset(),
+            eraser: () => eraser(),
+            res: () => {
+                acumulador.innerHTML += inpValor.innerHTML;
+                operacion(acumulador.innerHTML);
+            }
+        }
+        if (ACTIONS[valor]) { ACTIONS[valor]() } 
+        else if (operadores.some(e => e === valor)) {
             if (inpValor.innerHTML === '') { inpValor.innerHTML = 0 }
             acumulador.innerHTML += `${inpValor.innerHTML} ${valor} `;
             newOperation = true;
-            break;
-        default:
-            if (valor === 'res') {
-                acumulador.innerHTML += inpValor.innerHTML;
-                operacion(acumulador.innerHTML);
-            } else { inpValor.innerHTML += valor; }
+        } else { inpValor.innerHTML += valor; }
     }
 });
-
 function operacion(expresion) {
     if (inpValor.innerHTML !== '') {
         inpValor.innerHTML = eval(expresion);
